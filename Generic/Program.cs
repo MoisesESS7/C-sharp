01199e0fe9b618a5ce3ba59b853474f8dcf37477
Moise Silva <moisessilvagsp@outlook.com>
2023-11-01 20:26:39 -0300

    método generic com restrição

---
+++ Generic/Program.cs
@@ -0,0 +1,23 @@
+﻿using Generic.Entities;
+using Generic.Services;
+using System.Globalization;
+
+List<Product> list = new List<Product>();
+
+Console.Write("Enter N ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] vetProduct = Console.ReadLine().Split(',');
+    string name = vetProduct[0];
+    double price = double.Parse(vetProduct[1], CultureInfo.InvariantCulture);
+    list.Add(new Product(name, price));
+}
+
+var calculationService = new CalculationService();
+var product = calculationService.Max(list);
+
+Console.Write("Max:");
+Console.WriteLine(product);
+
---
+++ Generic/Services/CalculationService.cs
@@ -0,0 +1,24 @@
+﻿namespace Generic.Services
+{
+    internal class CalculationService
+    {
+        public T Max<T>(List<T> list) where T : IComparable
+        {
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("Empt list");
+            }
+
+            T max = list[0];
+            for(int i = 1; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
